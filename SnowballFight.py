c0ee2567a7c6adce45754e8728d592fdd2f75bf6
csfdtower1truck1engine1bc1squad1utilty1 <schulzejoshua494@gmail.com>
2024-12-05 10:55:11 -0700

    at stuff

--- SnowballFight.py
+++ SnowballFight.py
@@ -6,6 +6,7 @@
     Python: 3.13.0                 
 '''
 import random
+import time
 
 def main():
     # the main runner of the game
@@ -24,29 +25,63 @@ def main():
         players.append(nextplayer)
     players.remove("done")
 
+
+    mode = input(" Yo you  wnat to pick who you through it at yes or no?")
+
+    gameplay(name, players, mode)
+
     
-    while (len(players) > 1):
+  
+
+
+def gameplay(name, players, manual):
+
+      while (len(players) > 1):
     
         thrower = random.choice(players)
+        if(thrower == name ):
+            if( manual == "yes"):
+                target = input("who do you want toss at? ")
+                while (target == thrower):
+                     target = input ( " you picked you get some one new ")
 
-        target = random.choice (players)
+                 
 
-        while (target == thrower):
-            target = random.choice (players)
 
+            else:        #auto mode
+                    target = random.choice (players)
+
+                    while (target == thrower):
+                        target = random.choice (players)
+        else:        #auto mode
+                    target = random.choice (players)
+
+                    while (target == thrower):
+                        target = random.choice (players)
         print ( thrower + " is throwing a snow ball at " + target + "!")
+        print (" -- ")
+        print("-  -")
+        print(" -- ")
         
         hitnum = random.randrange(6)
 
         success = hitResult(hitnum)
 
+        time.sleep (2)
+
         if ( success == True):
+            
+            if ( target not in players):
+                 print ( " you hit a passer by you are now out!!")
+                 players.remove(thrower)
             print("it is a hit ! " + target + " is down")
             players.remove(target)
         else:
-            print("sadly, " + thrower + " has bad aim and missedj")
-    print (players[0] + " is the best snowballer is in all the land!!!!!!")
+            print("sadly, " + thrower + " has bad aim and missed!")
+      print (players[0] + " is the best snowballer is in all the land!!!!!!")
+
 def hitResult(hitNum):
+    
     # based on the number that is passed in, return True or False 
     # indicating if this was a hit or a miss
     if(hitNum == 3):
